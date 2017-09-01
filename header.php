90959637dc4ea052ac2db73e754e5dbe3e5af70f
MMXLV <elpaladindx@hotmail.com>
2017-09-01 20:33:10 -0300

    nuevo modelo

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/slider.css">
+    <title>Home</title>
+  </head>
+  <body>
+    <div class="login-head">
+      <span>EMAIL</span>
+      <span>SEARCH</span>
+    </div>
+    <div class="container">
+      <header class="main-head">
+        <div class="logo">
+          <span>E-Comerce logo</span>
+        </div>
+        <div>
+          <ul class="nav-bar-lg">
+            <li><a class="dropbtn" href="index.php">Home</a></li>
+            <li><a class="dropbtn" href="login.php">Login</a></li>
+            <li><a class="dropbtn" href="register.php">Register</a></li>
+            <li><a class="dropbtn" href="faqs.php">FAQs</a></li>
+          </ul>
+        </div>
+        <nav>
+          <ul class="nav-bar">
+            <li class="dropdown">
+              <button class="dropbtn">Menu</button>
+              <div class="dropdown-content">
+                <a href="index.php">Home</a>
+                <a href="faqs.php">FAQs</a>
+                <a href="login.php">Login</a>
+                <a href="register.php">Register</a>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </header>
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php include_once ("header.php") ?>
+      <section>
+        <div class="banner">
+          <img src="images/banner.jpg" alt="banner" width="100%">
+        </div>
+        <article class="main-product">
+          <img src="images/img6.jpg" alt="New product" width="100%">
+        </article>
+        <article class="products">
+          <img src="images/img1.jpg" alt="">
+          <img src="images/img2.jpg" alt="">
+          <img src="images/img3.jpg" alt="">
+          <img src="images/img4.jpg" alt="">
+        </article>
+        <article class="products-slider">
+          <div class="slideshow-container">
+            <div class="mySlides fade">
+              <div class="numbertext">1 / 3</div>
+              <img src="images/img5.jpg" style="width:100%">
+            </div>
+            <div class="mySlides fade">
+              <div class="numbertext">2 / 3</div>
+              <img src="images/img2.jpg" style="width:100%">
+            </div>
+            <div class="mySlides fade">
+              <div class="numbertext">3 / 3</div>
+              <img src="images/img3.jpg" style="width:100%">
+            </div>
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+          </div>
+          <br>
+          <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+          </div>
+        </article>
+      </section>
+      <script src="js/slider.js"></script>
+<?php include_once ("footer.php") ?>
